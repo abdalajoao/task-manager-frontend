202047bffeddb313f4fd71cf80b1f28241925796
João Abdala <isacabala@gmail.com>
2026-06-13 11:43:49 +0100

    files add

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
-const BASE_URL = "https://task-manager-server-n8j0.onrender.com";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
+
+console.log("BASE_URL =", BASE_URL);
 /* ───────────────────────────────
    GET ALL ACTIVITIES
 ─────────────────────────────── */
